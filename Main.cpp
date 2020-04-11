bb9cd06b25948f3f8ec6db431bc8623b78a69ed7
zaki <ahmedzakariya355@gmail.com>
2020-04-11 11:41:23 -0400

    Had a few difuclitesl with this proejct

--- Main.cpp
+++ Main.cpp
@@ -8,168 +8,137 @@
 #include <vector>
 #include <climits>
 #include <bitset>
-
-
-int SectorSize = 512;
-int SectorNum = 1000;
-int inodeCount = 21;
-
-
-
-
-
-#include <iostream>
-#include <cmath>
+#include <cassert>
+#include "Main.h"
 using namespace std;
 
-int main(){
-   inode temp;
-   temp.writeInode();
-    return 0;
-}
 
-// inode 
-//     - writeBitStream() :: Write teh type, size and allociation, by reversing the blwo opeariton
+#define SectorSize 512
+#define SectorNum 1000
+#define inodeCount 35
+#define inodeOffset 6
 
-//     - readBitStream() :: read the type, size and allcioation by folowing the following processess
-
-//     - There are 4 indoes within a inode sector. The makeup totals to 114 bits.
-//       - 1 bit  :: for which type of inode this is.
-//       - 13 bits :: (or 1.625 bytes) for representing the size of datablocks
-//       - 100 bits ::  10 seqeunces of 10 bits for reprsenting the location. note that all 1s mean that this is not allociated
-	
-//     - This results of 106 of useless data, and 3990 of useful data. Since there are 35 inodes in a sector, we split it up into an array, with each piece being a substr of 114 bits.
-
-//     - The function below is a method ofreading it. Note it doesn't return anything. Maybe i'll try to do that thing where i have an inlnie function and do it there.
-
-//     - Anotehr note: there'll be 35 inodes withn a sector, so the spliting of that by 114 is left to futrue zak.
-
-//     - Writing it to bitstream is simple. if need be write a funciton for it.
-
-//       #+NAME: bitstreamtoInode
-//       #+HEADER: :noweb yes :tangle Main.cpp   :colnames no :comments org
-//      #+HEADER: :includes "<iostream> <cmath> <vector> <climits> <bitset>"
-
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <climits>
-#include <bitset>
-
-
-
-
-
-
-int main() {
-#include<iostream>
-#include<bitset>
-using namespace std;
-// Note in babel mode this will be incorrect
-
-void readBitDataInode(){// Type // Size // 10*10 of which bits are allociated to it.
-
-    // This little test is used to demonstrate values used to finding where to substring
-    /*
-    string test= "11111NNNNN22222NNNNN33333NNNNN44444NNNNN55555NNNNN66666NNNNN77777NNNNN88888NNNNN99999NNNNN00000NNNNNSSSSSSSSSSSSST";
-    cout << test.substr(0,100) << endl; //Which are allcoiated
-    cout << test.substr(100,13) << endl; //Size
-    cout << test.substr(113,1) << endl; //Type
-    for(int i=0; i<10; i++){ 
-	cout << test.substr(i*10,10) << endl;  // used to show how to splti the function
-    }
-    */
-
-
-    string test= "001001010000000000000000000000000000000000000000000000000000000000000000000000000000000010000000001100000001111111";
-	uint alloc[10];
-	for(int i=0; i<10; i++){
-		bitset<10> temp(test.substr(i*10, 10));
-		cout << temp << '\t' << temp.to_ulong() << endl;
-		alloc[i]=temp.to_ulong();
-	}
-	uint size;
-	bitset<13> temp2(test.substr(100,13));
-	size=temp2.to_ulong();
-
-	bool type;
-	bitset<1> temp3(test.substr(113,1));
-	type=temp2.to_ulong();
-
-	cout << size << '\t' << type << endl;
-}
-return 0;
-}
-
-// datablock
-//     - Datablocks are disgshiustehd by two types: file and directory
-//     - the type of the datablock is denoted by teh inode, not the directory.
-//     - For directory, tehre is a 20 bytes/160 bits, which are
-//       + 16 bytes/128 bits :: file name. 15 characters PLUS 1 for end of string, so it's mroe of 15 characters
-//       + 4 byte/32 bits :: inode that shows which file/driectory this is.
-//     - This means that dictionaries cna have 25 files in a a sector, but 250 files/directories overall.
-//     - This doesn't have the case, of half a directoriy's infroamtion being in one datablcok, and the other half being in another datablock. THat isn't consdiered.
-//      #+NAME: bitstreamtoInode
-//      #+HEADER: :noweb yes :tangle Main.cpp   :colnames no :comments org 
-//      #+HEADER: :includes "<iostream> <cmath> <vector> <climits> <bitset>"
-
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <climits>
-#include <bitset>
-
-
-
-
-
-
-using namespace std;
-void readDir(string TestString){
-    bitset<64> inode(TestString.substr(0,4));
-    cout << inode.to_ulong() << endl;
-    char temp[10];
-    for(int i=0; i<8; i++){
-	    bitset<16> temp(TestString.substr(4+i*16,16));
-	    cout << (char)temp.to_ulong() << endl;
-
-    }
-
-}
-
-int main(){
-/*
-	string temp1="iiii11111111111111112222222222222222333333333333333344444444444444445555555555555555666666666666666677777777777777778888888888888888";
-	for(int i=0; i<8; i++){
-	    cout << temp1.substr(4+i*16,16) << endl;
-	}
-*/
-	/*readDir("111110000000000000001000000000000000100000000000000010000000000000001000000000000000100000000000000010000000000000001000000000000000");*/
-	readDir("111100000000010000010000000001000010000000000100001100000000010001000000000001000101000000000100011000000000010001110000000001001000");
-}
-
-// Sector/Root Inode      
-//     - A sector is a collection of a superblock, bitmaps for in use indoes and datablocks, a sqeunce of indoes, and a sequence of datablocks.  However, this information HAS TO BE CONVERETD to that. Otehrwise, a sector is just an array of bitsets of 4096 bits.
-//     - However, the sector converts it's concats to usuable datasturcutres. After each file/directory operation, it saves the stuff to workign directory. Than, working directory saves it stuff to external disk when FS_SYNC() is made.
-//     - The disks are just a bitset array of 4096 bits, with 1000 elements in each.
-//     - The root inode is the indoe that represtns nothing. This is a special variable, as to not have to find out what it is on disk tediously.
-      
-//    #+HEADER: :noweb yes :tangle Main.cpp   :colnames no :comments org
-// #+HEADER: :includes "<iostream> <cmath> <vector> <climits> <bitset>"
-
-#include <iostream>
-#include <cmath>
-#include <vector>
-#include <climits>
-#include <bitset>
-
-
-
-
-
-
-int main() {
+//THe Disk
 std::bitset<4096> ExtDisk[1000];
 std::bitset<4096> WorkDisk[1000];
-return 0;
+
+
+
+
+
+struct inode{
+     bool type;
+     uint size;
+     uint alloc[10];
+};
+struct dir{
+     char Name[16];
+     uint long inodePlace;
+};
+
+inode readBitDataInode(string BitStream){// Type // Size // 10*10 of which bits are allociated to it.
+
+     inode Ret;
+     { // alloc decleartion
+
+	  for(int i=0; i<10; i++){
+	       bitset<10> temp(BitStream.substr(i*10, 10));
+	       Ret.alloc[i]=temp.to_ulong();
+	  }
+     }
+     { // SIze decleartion
+	  bitset<13> temp(BitStream.substr(100,13));
+	  Ret.size=temp.to_ulong();
+     }
+
+     {// Type decleartion
+	  bitset<1> temp(BitStream.substr(113,1));
+	  Ret.type=temp.to_ulong();
+     }
+     return Ret;
 }
+
+bitset<114> writeBitDataInode(inode Inode){//Retrun 114 bits
+
+     bitset<114> Ret;
+     string RetStr; //So we can concate instead of dealing with how bits are setup in bitset
+     { // alloc decleartion
+
+	  for(int i=0; i<10; i++){ // Loop through each alloc
+	       bitset<10> tempAlloc(Inode.alloc[i]);
+	       for(int j=0; j<10; j++){ // Loop through each value in alloc
+		    int val=9-j;
+		    cout << (i*10+j) << ' ' << tempAlloc[val] << '\t';
+		    Ret.set(i*10+j,tempAlloc[val]);
+		    
+	       }
+	       cout << endl;
+
+	  }
+     }
+     /*
+     { // SIze decleartion
+	  bitset<13> temp(Inode.size);
+	  for(int i=100; i<100+13; i++){
+	       Ret[i]=temp[i-100];
+	       
+	  }
+     }
+
+     {// Type decleartion
+	  bitset<1> temp(Inode.type);
+	  Ret[113]=temp[0];
+     }
+     */
+     bitset<114> Ret;
+     return Ret;
+}
+
+
+
+
+//Expect 20 bytes, or 16 characters of 8 bits, and 4 bits for size
+dir readDir(string BitStream){
+     dir Ret;
+     { // inodePlace decleariotn
+	  bitset<4> inode(BitStream.substr(0,4));
+	  uint long ret1=inode.to_ulong();
+	  Ret.inodePlace=ret1;
+     }
+     { // Name assigmetnation
+	  for(int i=0; i<16; i++){
+	       bitset<8> temp(BitStream.substr(4+i*8,8));
+	       Ret.Name[i]=(char)temp.to_ulong();
+
+	  }
+     }
+     return Ret;
+
+}
+
+int main(){
+		string test= "100000000010000000001000000000100000000010000000001000000000100000000010000000001000000000100000000010000000000000";
+		inode temp=readBitDataInode(test);
+
+		bitset<114> test2=writeBitDataInode(temp);
+		cout << "Input:\t" << test << endl;
+		cout << "Output:\t" << test2 << endl;
+
+		cout << "Inode size and type:\t" << temp.size << '\t' << temp.type << endl;
+		for(int i=0; i<10; i++){
+		     cout << temp.alloc[i] << '\t';
+		}
+		cout << endl;
+
+		inode temp=readBitDataInode(test);
+		cout << "Inode size and type:\t" << temp.size << '\t' << temp.type << endl;
+		for(int i=0; i<10; i++){
+		     cout << temp.alloc[i] << '\t';
+		}
+		cout << endl;
+
+		
+		//assert(test2.to_string()==test);
+
+}
+
